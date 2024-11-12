978bbd6b864befdf9371ba0ca920f0834a1f04a8
profe-ricardo <ricardo.gutierrez22@inacapmail.cl>
2024-11-12 19:34:25 -0300

    habia faltado el chef

---
+++ models/people/chef.py
@@ -0,0 +1,8 @@
+class Chef:
+    def __init__(self, name, id, location):
+        self.name = name
+        self.id = id
+        self.location = location
+    
+    def take_orders(self):
+        pass
